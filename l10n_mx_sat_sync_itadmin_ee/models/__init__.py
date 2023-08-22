cc3213b6a7b9ee8f74066ec2f4c6414f6cb235f2
Elisa Cervantes <elisacervantes@Elisas-MacBook-Air.local>
2023-08-22 17:31:19 -0600

    [REV] code

--- l10n_mx_sat_sync_itadmin_ee/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import account_invoice
-from . import esignature
-from . import ir_attachment
-from . import res_company
-from . import res_config_settings
-from . import purchase_order
-from . import account_payment
