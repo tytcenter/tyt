3c023b0c323d9eef5b92c86eadbc12743903930d
tytcenter <58714871+tytcenter@users.noreply.github.com>
2024-10-09 14:51:18 -0600

    Add files via upload
    Signed-off-by: tytcenter <58714871+tytcenter@users.noreply.github.com>

---
+++ nomina_cfdi_extras_ee/models/res_config_settings.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = 'res.config.settings'
+
+    numoer_de_retardos_x_falta = fields.Integer("Número de retardos x falta")
+    riesgo_de_trabajo=fields.Many2one('hr.leave.type',related='company_id.leave_type_rie_id', string='Riesgo de trabajo',readonly=False)
+    enfermedad_general=fields.Many2one('hr.leave.type',related='company_id.leave_type_enf_id', string='Enfermedad general',readonly=False)
+    maternidad=fields.Many2one('hr.leave.type',related='company_id.leave_type_mat_id', string='Maternidad',readonly=False)
+
+    falta_just_con =fields.Many2one('hr.leave.type',related='company_id.leave_type_fjc', string='Falta justificada con sueldo',readonly=False)
+    falta_just_sin =fields.Many2one('hr.leave.type',related='company_id.leave_type_fjs', string='Falta justifica sin sueldo',readonly=False)
+    falta_injustificada =fields.Many2one('hr.leave.type',related='company_id.leave_type_fi', string='Falta injustificada',readonly=False)
+    falta_retardo =fields.Many2one('hr.leave.type',related='company_id.leave_type_fr', string='Falta por retardo',readonly=False)
+    vacaciones = fields.Many2one('hr.leave.type',related='company_id.leave_type_vac', string='Vacaciones',readonly=False)
+    festivo_doble =fields.Many2one('hr.leave.type',related='company_id.leave_type_dfes', string='Festivo doble',readonly=False)
+    festivo_triple =fields.Many2one('hr.leave.type',related='company_id.leave_type_dfes3', string='Festivo triple',readonly=False)
+
+    vacaciones_adelantadas = fields.Boolean(string='Vacaciones adelantadas')
+
+    @api.model
+    def get_values(self):
+        res = super(ResConfigSettings, self).get_values()
+        params = self.env['ir.config_parameter'].sudo()
+        res.update(
+            numoer_de_retardos_x_falta=int(params.get_param('nomina_cfdi_extras_ee.numoer_de_retardos_x_falta', 0)),
+            vacaciones_adelantadas = params.get_param('nomina_cfdi_extras_ee.vacaciones_adelantadas'),
+        )
+        return res
+
+    def set_values(self):
+        res = super(ResConfigSettings, self).set_values()
+        self.env['ir.config_parameter'].sudo().set_param('nomina_cfdi_extras_ee.numoer_de_retardos_x_falta', self.numoer_de_retardos_x_falta)
+        self.env['ir.config_parameter'].sudo().set_param('nomina_cfdi_extras_ee.vacaciones_adelantadas', self.vacaciones_adelantadas)
+        return res
+    
