acab0c29514a91d8fcbaf6374fb36421f5b90817
tytcenter <58714871+tytcenter@users.noreply.github.com>
2024-10-09 14:47:52 -0600

    Add files via upload
    Signed-off-by: tytcenter <58714871+tytcenter@users.noreply.github.com>

---
+++ nomina_cfdi_sbc/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Nomina SBC",
+    "author": "IT Admin",
+    "version": "16.02",
+    "category": "Other",
+    "description":"Calcula el SBC bimestral.",
+    "depends": ["nomina_cfdi_extras_ee"],
+    "data": [
+        "security/ir.model.access.csv",
+        "wizard/wizard_sbc_bimestral.xml",
+        
+    ],
+    "license": 'AGPL-3',
+    'installable': True,
+    'images': [''],
+}
